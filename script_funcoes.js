493d4bd167e3dec0470ea6d67792ecd740ef3187
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2020-07-22 11:01:06 -0300

    criacao dos comandos

---
+++ script_funcoes.js
@@ -0,0 +1,29 @@
+//Função IF ELSE
+
+/*
+function retornaSexo(sexo){
+    if (sexo === 'M'){
+        return 'Masculino';
+    } else if (sexo === 'F'){
+        return 'Feminino'
+    } else{
+        return 'Outro'
+    }
+}
+*/
+
+//Função Switch
+/*
+function retornaSexo(sexo) {
+    switch (sexo) {
+        case 'M':
+            return 'Masculino';
+        case 'F':
+            return 'Feminino';
+        default:
+            return 'Outro';
+    }
+}
+*/
+var resultado = retornaSexo('M');
+console.log(resultado);
---
+++ script_variaveis-numericos.js
@@ -0,0 +1,23 @@
+//JS trabalha com variáveis dinâmicas
+/*
+var nome = 'Viviane'; //automaticamente String
+var idade = 31; //automaticamente Inteiro
+var peso = 60; //automaticamente Float
+var humano = true; //automaticamente Boolean
+*/
+console.log(humano);
+/*
+//Array (vetores)
+var pessoas = ['Marco', 'Viviane', 'Pitoxe']
+*/
+console.log(pessoas[1]);
+/*
+//Objetos
+var pessoa = {
+    nome: 'Marco',
+    idade: 30,
+    peso: 84.1,
+    humano: true
+};
+*/
+console.log(pessoa.nome);
