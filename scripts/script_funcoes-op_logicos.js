447f633ab94ad1ae156df8e5e8bfd56ccc045dd3
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2020-07-23 09:07:56 -0300

    alt arquivos

--- scripts/script_funcoes-op_logicos.js
+++ scripts/script_funcoes-op_logicos.js
@@ -41,3 +41,26 @@ var resultado = x+y; //15 -> soma
 */
 /*********************************************************************************************** */
 //console.log(resultado);
+/*############################################################################################## */
+//Operadores Lógicos 
+//(==)-> confere o valor variável '5' == '6' -> retorna false; pois os valore são diferente, apensar de ambos serem SRING. 5=='5'-> retorna TRUE, pois o valor é o mesmo, apesar de um ser INT e outro STRING.
+//(===)-> confere o valor interno da variável e o tipo  5==='5'-> retorna FALSE, mesmo valor, mas tipos diferentes, INT e STRING
+/*
+var sexo = 'M', idade = 23;
+if (sexo === 'M' && idade >= 18){ //&& = AND
+    console.log('OK');
+}
+*/
+
+/*
+var sexo = 'M', idade = 23;
+if (sexo === 'M' || idade >= 18){ //|| = OR
+    console.log('OK');
+}
+*/
+/*
+var sexo = 'F';
+if (sexo !== 'M'){ //! = NO
+    console.log('OK');
+}
+*/
